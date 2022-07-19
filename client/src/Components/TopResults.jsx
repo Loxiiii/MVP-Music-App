9b755f1b4aa79f06c3c337152aa359662fef2169
Javi <javiercamposcastello@gmail.com>
2022-07-18 21:46:42 -0500

    Working on fetching top USA

--- client/src/Components/TopResults.jsx
+++ client/src/Components/TopResults.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+import MUSIC_MATCH_KEY from '../../../config.js'
+
+const topUsURL =  'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1'
 
 class TopResults extends React.Component {
   constructor (props) {
@@ -6,6 +10,16 @@ class TopResults extends React.Component {
     this.state = {
       top:[]
     };
+  }
+
+  componentDidMount() {
+    console.log('This is the key: ', MUSIC_MATCH_KEY);
+    axios.get(topUsURL + MUSIC_MATCH_KEY)
+    .then((result) => {
+      console.log(result.data);
+    }).catch((e) => {
+      console.log(e);
+    })
   }
 
   render () {
